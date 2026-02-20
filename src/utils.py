693b798761e434be98e2bdb63778932a57811e34
casten <gabrielepaulon01@gmail.com>
2026-02-20 02:12:08 +0100

    Added function to calculate weights of loss

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,12 @@
 import numpy as np
 import pandas as pd
 from .config import Config
+import torch
+import json
+import numpy as np
+from collections import Counter
+from tqdm import tqdm
+from pathlib import Path
 
 def discretize_location(x, y):
     """
@@ -35,3 +41,59 @@ def discretize_duration(seconds):
     if seconds < 6.0: return 7  
     if seconds < 10.0: return 8 
     return 9                    
+
+def calculate_and_print_weights():
+    print(f"Loading data from {Config.PROCESSED_DATA_DIR / 'dataset_tokenized.pt'}...")
+    try:
+        dataset = torch.load(Config.PROCESSED_DATA_DIR / "dataset_tokenized.pt", weights_only=False)
+        
+        with open(Config.PROCESSED_DATA_DIR / "vocab.json", 'r') as f:
+            full_vocab = json.load(f)
+        type_vocab = full_vocab['type_name']
+        
+    except FileNotFoundError as e:
+        print(f"Error: files not found.\n{e}")
+        return
+
+    print("Counting frequencies...")
+    counts = Counter()
+    
+    for item in tqdm(dataset, desc="Scanning"):
+        counts.update(item['type_ids'])
+
+    max_count = max(counts.values())
+    total_samples = sum(counts.values())
+    
+    print(f"\nTotal Events: {total_samples}")
+    print(f"Max Frequency (Class '{[k for k,v in type_vocab.items() if v==counts.most_common(1)[0][0]][0]}'): {max_count}")
+
+    weight_dict = {}
+    
+    print("\n--- WEIGHTS ---")
+    print(f"{'ACTION':<20} | {'COUNT':<10} | {'WEIGHT':<10}")
+    print("-" * 45)
+
+    for name, idx in type_vocab.items():
+        if idx in counts:
+            count = counts[idx]
+            # Formula Inverse Frequency: Max / Count
+            w = max_count / (count + 1e-6)
+            
+            # Clamping to 100.0 for stability
+            w = min(w, 100.0)
+            w = round(w, 2)
+            weight_dict[name] = w
+            
+            if count > 0:
+                print(f"{name:<20} | {count:<10} | {w:<10}")
+        else:
+            weight_dict[name] = 1.0
+
+    print("\n\n" + "="*50)
+    print("="*50)
+    print("MANUAL_WEIGHTS = {")
+    for k, v in weight_dict.items():
+        if v > 1.0:
+            print(f"    '{k}': {v},")
+    print("}")
+    print("="*50)
